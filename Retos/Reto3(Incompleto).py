340933a608994937abc55026adad5d8c504842db
migueldoradooo <migueldoradomoreno57@gmail.com>
2026-04-16 10:23:37 +0000

    Retos Hechos, contado el Extra

--- Retos/Reto3(Incompleto).py
+++
@@ -1,9 +0,0 @@
-num1 = int(input('Ingresa el primer numero: '))
-num2 = int(input('Ingresa el primer numero: '))
-num3 = int(input('Ingresa el primer numero: '))
-num4 = int(input('Ingresa el primer numero: '))
-num5 = int(input('Ingresa el primer numero: '))
-
-
-
-                          
--- Retos/Reto8.py
+++ Retos/Reto8.py
@@ -1,1 +1,12 @@
 #Número mayor
+
+numero = int(input("Introduce un número (0 para terminar): "))
+mayor = numero
+
+while numero != 0:
+    if numero > mayor:
+        mayor = numero
+    
+    numero = int(input("Introduce otro número (0 para terminar): "))
+
+print("El número más grande es:", mayor)

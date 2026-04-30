fa15aba3305e362fe25a80df665a879eb6c65315
migueldoradooo <migueldoradomoreno57@gmail.com>
2026-04-30 12:18:02 +0200

    Add files via upload

---
+++ Examen Repaso si y bucles/Ejercicio 1.py
@@ -0,0 +1,11 @@
+#di si la t emperatura es fria o agradable, si es menor a 15 grados es fria, si no es agradable
+
+
+temperatura = int(input("Dime una temperatura: "))
+
+if temperatura < 15:
+    print('frio')
+else:
+    print('Agradable')
+    
+    
